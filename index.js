24c5a22919330e4c25fb2c05dee5eef2c4e1c994
rmnmatusevich <rmnmatusevich@gmail.com>
2020-04-15 23:17:47 +0300

    Add server-side

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const obj = {
   collage: [
     {
@@ -48,20 +51,50 @@ const obj = {
     },
   ],
 };
+
 app.use(express.static(path.join(__dirname, "/client/public/index.html")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/public/index.html"));
-// });
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/public/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 app.get("/getCollage", (req, res) => {
+  console.log(req);
+
+  res.json(obj);
+});
+
+app.post("/login", (req, res) => {
+  if (req.body.username === "admin" && req.body.password === "1234") {
+    res.json({ auth: true });
+  } else {
+    res.json({ auth: false });
+  }
+});
+
+app.post("/searchCollage", (req, res) => {
+  const newCollageObj = [];
+  for (let item of obj.collage) {
+    if (item.description.toLocaleLowerCase().includes(req.body.input)) {
+      newCollageObj.push(item);
+    }
+  }
+  res.json(newCollageObj);
+});
+app.post("/addCollage", (req, res) => {
+  obj.collage.push({
+    src: req.body.image,
+    tittle: req.body.tittle,
+    description: req.body.description,
+  });
   res.json(obj);
 });
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log("App is listening on port " + port);
+});
+
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
