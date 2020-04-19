c6c97b22c1e8cdd99d35c8dc5d92d39c207390ef
rmnmatusevich <rmnmatusevich@gmail.com>
2020-04-19 15:11:34 +0300

    Add validation to sign up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -8,6 +8,7 @@ import {
   setLastNameThunk,
   setAgeThunk,
 } from "../redux/actions/signUpActions";
+
 function SignUp() {
   const dispatch = useDispatch();
   const username = useSelector((state) => state.signUp.signUpUsername);
@@ -17,7 +18,7 @@ function SignUp() {
   );
   const firstName = useSelector((state) => state.signUp.signUpFirstName);
   const lastName = useSelector((state) => state.signUp.signUpLastName);
-  const redirect = useSelector((state) => state.signUp.signUpAge);
+  const age = useSelector((state) => state.signUp.signUpAge);
 
   return (
     <section className="sign-up">
@@ -26,36 +27,51 @@ function SignUp() {
         <label className="sign-up-form__label">Username</label>
         <input
           id="sign-up-form__username"
+          minLength="3"
           onChange={(event) => dispatch(setUsernameThunk(event.target.value))}
+          required
         ></input>
         <label className="sign-up-form__label">Password</label>
         <input
           id="sign-up-form__password"
           type="password"
+          pattern="^(?=.*\d)(?=.*[a-z])(?!.*\s).*$"
+          title="Введите как минимум 1 цифру и 1 букву."
           onChange={(event) => dispatch(setPasswordThunk(event.target.value))}
+          required
         ></input>
         <label className="sign-up-form__label">Repeat password</label>
         <input
           id="sign-up-form__password-repeat"
           type="password"
+          pattern={password}
+          tittle="Пароли не совпадают."
           onChange={(event) =>
             dispatch(setPasswordRepeatThunk(event.target.value))
           }
+          required
         ></input>
         <label className="sign-up-form__label">First Name</label>
         <input
           id="sign-up-form__firstName"
+          minLength="3"
           onChange={(event) => dispatch(setFirstNameThunk(event.target.value))}
+          required
         ></input>
         <label className="sign-up-form__label">Last Name</label>
         <input
           id="sign-up-form__lastName"
+          minLength="3"
           onChange={(event) => dispatch(setLastNameThunk(event.target.value))}
+          required
         ></input>
         <label className="sign-up-form__label">Age</label>
         <input
           id="sign-up-form__age"
+          type="number"
+          min="1"
           onChange={(event) => dispatch(setAgeThunk(event.target.value))}
+          required
         ></input>
         <button id="sign-up-submit">Sign Up</button>
       </form>
